44436d482d1171bac440deaa7a08b0f41d890769
puja sarangdhar bajode <pujabajode692@12345>
2023-10-17 23:16:53 -0600

    update

---
+++ Day-1-JS/Day-4-JS/Day-4-JS.js
@@ -0,0 +1,21 @@
+
+
+function add(p,v){
+
+console.log("add",p+v);
+console.log("subt",p-v);
+console.log("mult",p*v);
+console.log("divd",p/v);
+console.log("mod",p%v);
+
+}
+
+
+
+add(20,50)
+
+
+
+
+
+
---
+++ Day-1-JS/Day-4-JS/Day-5-JS/Day-5-JS.js
@@ -0,0 +1,8 @@
+var subjects =["html", "css", "javascript", "java", ];
+
+for(i=0; i<subjects.length; i++);{
+
+console.log("subjects"[i]);
+
+}
+
